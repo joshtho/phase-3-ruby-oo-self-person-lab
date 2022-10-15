0fe0e7132dafd1c7f5ce7abd366adbe0465573df
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-10-15 14:42:28 -0500

    commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,22 @@
 # your code goes here
+class Person 
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, bank_account = 25) 
+        @name = name
+        @bank_account = bank_account
+    end
+
+    def bank_account(bank_account = 25) 
+        @bank_account = bank_account
+    end
+
+    def happiness(happiness = 8) 
+        @happiness = happiness
+    end
+
+    def bank_account(bank_account = 8) 
+        @bank_account = bank_account
+    end
+end
